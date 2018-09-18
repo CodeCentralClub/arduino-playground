d195a1581347e889d6dc2aa2dae0a5ea973e4d1c
Eric <xeno20neko@gmail.com>
2018-09-18 14:16:55 -0600

    feat: Initial commit

---
+++ arduino all/sketch_sep18a/sketch_sep18a.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A1,INPUT_PULLUP);
+  digitalWrite(A1,HIGH);
+  pinMap(B1,OUTPUT);
+  pinMap(
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
